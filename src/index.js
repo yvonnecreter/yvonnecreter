57d9b0ec696d6a88c598a7109d318b668f692c79
Yvonne Creter <57372700+yvonnecreter@users.noreply.github.com>
2024-07-05 17:37:51 +0200

    feat: classbased popup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const buttonEle = document.getElementById("trigger")
+// const buttonEle = document.getElementById("trigger")
+// const closeButtonEle = document.getElementById("close")
+// const modalEle = document.getElementById("popup")
+
+// buttonEle.addEventListener("click", (event) => {
+//     modalEle.classList.toggle("hidden")
+// })
+// closeButtonEle.addEventListener("click", (event) => {
+//     modalEle.classList.toggle("hidden")
+// })
+
 const closeButtonEle = document.getElementById("close")
 const modalEle = document.getElementById("popup")
-console.log(buttonEle)
-buttonEle.addEventListener("click", (event) => {
-    modalEle.classList.toggle("hidden")
-})
-closeButtonEle.addEventListener("click", (event) => {
-    modalEle.classList.toggle("hidden")
-})
+
+document.querySelectorAll('.trigger').forEach(buttonEle => {
+    buttonEle.addEventListener("click", (event) => {
+        modalEle.classList.remove("hidden");
+    });
+
+    closeButtonEle.addEventListener("click", (event) => {
+        modalEle.classList.add("hidden");
+    });
+});
