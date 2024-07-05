6e3f77e3f89e14bf09690922a155a45ec162e284
Yvonne Creter <57372700+yvonnecreter@users.noreply.github.com>
2024-07-05 15:21:45 +0200

    feat: popup logic

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const buttonEle = document.getElementById("trigger")
+const closeButtonEle = document.getElementById("close")
+const modalEle = document.getElementById("popup")
+console.log(buttonEle)
+buttonEle.addEventListener("click", (event) => {
+    modalEle.classList.toggle("hidden")
+})
+closeButtonEle.addEventListener("click", (event) => {
+    modalEle.classList.toggle("hidden")
+})
